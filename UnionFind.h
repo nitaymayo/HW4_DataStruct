05f068fefd8fc97a66f865516b9c53ba049fb2be
nitaymayo <nitaymayo@gmail.com>
2025-01-10 16:28:57 +0200

    UnionFind.h initial implementation

--- UnionFind.h
+++ UnionFind.h
@@ -11,11 +11,11 @@ template<class T>
 class Group{
   private:
     int m_size;
-    int ID;
+    int m_ID;
     shared_ptr<T> head;
 
   public:
-    Group<T>(int ID, shared_ptr<T> member): m_size(1), ID(ID), head(member){};
+    Group<T>(int ID, shared_ptr<T> member): m_size(1), m_ID(ID), head(member){};
 
     ~Group() = default;
 
@@ -26,10 +26,10 @@ class Group{
       this->m_size = size;
     }
     int getID(){
-      return ID;
+      return m_ID;
     }
     void setID(int ID){
-      this->ID = ID;
+      this->m_ID = ID;
     }
     shared_ptr<T> getHead(){
       return head;
@@ -40,15 +40,49 @@ class Group{
 };
 
 template<class T>
+class RevTreeNode{
+  private:
+    int m_ID;
+    shared_ptr<T> m_data;
+    RevTreeNode<T>* parent;
+    public:
+      RevTreeNode(int ID, shared_ptr<T> data, RevTreeNode* parent):
+      m_ID(ID), m_data(data), parent(parent){};
+
+      ~RevTreeNode() = default;
+
+      int getID(){
+        return m_ID;
+      }
+      void setID(int ID){
+        this->m_ID = ID;
+      }
+      shared_ptr<T> getData(){
+        return m_data;
+      }
+      void setParent(RevTreeNode* parent){
+        this->parent = parent;
+      }
+};
+
+template<class T>
 class UnionFind {
   private:
-    DynamicArray<T> groups;
-    DynamicArray<T> members;
+    DynamicArray<Group<T>> groups;
+    DynamicArray<T> nodes;
 
   public:
+    UnionFind(int size): groups(), nodes(){}
+
+    ~UnionFind() = default;
+
+    bool makeSet(T data){
+      Group<T> group(0, data);
+      RevTreeNode<T> node = new RevTreeNode<T>(data.getID(), data, &group);
+      int nodeID = nodes.insert(data), groupID = groups.insert(group);
+    }
 
 
-
-}
+};
 
 #endif //UNIONFIND_H
